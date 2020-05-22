4c576a1380ef14f777f3da1dd4586100d645a69e
nfoos <nfoos@rm-f.me>
2020-05-22 05:08:05 +0000

    force ssl

--- config/prod.exs
+++ config/prod.exs
@@ -11,6 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :menu_planner, MenuPlannerWeb.Endpoint,
   url: [host: System.get_env("HOST"), port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
